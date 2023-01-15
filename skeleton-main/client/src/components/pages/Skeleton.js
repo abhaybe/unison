155a3ba0833d934ce3230eeeba23ac881a34407c
ofoofoo <ofoo@mit.edu>
2023-01-15 16:13:48 -0500

    add CLIENT_ID

--- skeleton-main/client/src/components/pages/Skeleton.js
+++ skeleton-main/client/src/components/pages/Skeleton.js
@@ -5,7 +5,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "FILL ME IN";
+const GOOGLE_CLIENT_ID = "808533395363-h4ejmvg7ut0loi69pt6pqacr16mtuiqm.apps.googleusercontent.com";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   return (
