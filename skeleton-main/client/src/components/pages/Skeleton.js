880188b1b50d3dcedc856362797199e8da8fab85
Abhay Bestrapalli <abhaybestx@gmail.com>
2023-01-18 19:29:04 -0500

    added createlobby and joinlobby

--- skeleton-main/client/src/components/pages/Skeleton.js
+++ skeleton-main/client/src/components/pages/Skeleton.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 import "../../utilities.css";
+import CreateLobby from "../modules/CreateLobby.js";
+import JoinLobby from "../modules/JoinLobby.js";
 import "./Skeleton.css";
 
 const Skeleton = () => {
-  return <h1>orion and abhay say hi</h1>;
+  return (
+    <>
+      {" "}
+      <h1>orion and abhay say hi</h1>
+      <CreateLobby />
+      <JoinLobby />
+    </>
+  );
 };
 
 export default Skeleton;
