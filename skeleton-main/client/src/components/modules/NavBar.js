0f7a6d2c7da783cfc36b9f2d66a43aa4d93a1713
ofoofoo <ofoo@mit.edu>
2023-01-18 19:08:53 -0500

    Move Google Oauth to Navbar

---
+++ skeleton-main/client/src/components/modules/NavBar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "@reach/router";
+import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
+
+import "./NavBar.css";
+
+const GOOGLE_CLIENT_ID = "808533395363-h4ejmvg7ut0loi69pt6pqacr16mtuiqm.apps.googleusercontent.com";
+
+/**
+ * The navigation bar at the top of all pages. Takes no props.
+ */
+const NavBar = ({ userId, handleLogin, handleLogout }) => {
+  return (
+    <nav className="NavBar-container">
+      <div className="NavBar-title u-inlineBlock">
+        <Link to="/" className="NavBar-link">
+          Unison
+        </Link>
+      </div>
+      <div className="NavBar-linkContainer u-inlineBlock">
+        <Link to="/" className="NavBar-link">
+          Home
+        </Link>
+        {/* <Link to="/profile/" className="NavBar-link">
+          Profile
+        </Link>
+        <Link to="/leaderboard/" className="NavBar-link">
+          Leaderboard
+        </Link> */}
+        <div className="NavBar-link">
+          <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            {userId ? (
+              <button
+                onClick={() => {
+                  googleLogout();
+                  handleLogout();
+                }}
+              >
+                Logout
+              </button>
+            ) : (
+              <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
+            )}
+          </GoogleOAuthProvider>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
