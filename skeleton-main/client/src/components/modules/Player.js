150bb6e671c2017357b6b8aff6a44e74f788b5c5
Abhay Bestrapalli <abhaybestx@gmail.com>
2023-01-22 17:07:51 -0500

    added an api to lobby.js

---
+++ skeleton-main/client/src/components/modules/Player.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "./Player.css";
+
+const Player = (props) => {
+  return <>{props.user}</>;
+};
+
+export default Player;
--- skeleton-main/client/src/components/pages/Lobby.js
+++ skeleton-main/client/src/components/pages/Lobby.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "@reach/router";
 import { get } from "../../utilities";
+import PlayerList from "../modules/PlayerList";
 
 const Lobby = () => {
   const [Lobbyinfo, setLobby] = useState();
 
   useEffect(() => {
-    get("/api/lobby");
+    get("/api/lobby", {lobbyName:}).then((lobby)=> {setLobby(lobby)})
   });
 
-  return <></>;
+  return (
+    <>
+      {" "}
+      <PlayerList playerlist={Lobbyinfo} />
+    </>
+  );
 };
 
 export default Lobby;
