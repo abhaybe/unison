365b6151dc85edacebd455442b4f3cb30b6b9cce
Abhay Bestrapalli <abhaybestx@gmail.com>
2023-01-23 20:02:44 -0500

    not working dont come here again

--- skeleton-main/client/src/components/modules/Player.js
+++ skeleton-main/client/src/components/modules/Player.js
@@ -1,8 +1,17 @@
+import { get } from "mongoose";
 import React, { useState, useEffect } from "react";
 // import "./Player.css";
 
 const Player = (props) => {
-  return <>{props.user}</>;
+  // const [userName, setName] = useState("63cf1466d36fb932149cd4ae");
+
+  // useEffect(() => {
+  //   get("/api/getuser", { userId: props.user }).then((userr) => {
+  //     console.log(user);
+  //     setName(userr.username);
+  //   });
+  // }, [props.user]);
+  return <>{props.userId}</>;
 };
 
 export default Player;
