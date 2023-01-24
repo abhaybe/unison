30674fa4143562fb40783e5dc6eea0c6aa95408b
ofoofoo <ofoo@mit.edu>
2023-01-24 01:51:15 -0500

    deployment

--- skeleton-main/server/server.js
+++ skeleton-main/server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 /*
 |--------------------------------------------------------------------------
 | server.js -- The core of your server
@@ -33,8 +34,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL =
-  "mongodb+srv://unison:mV7C2uOhKlR4OijP@unison.zcwofrs.mongodb.net/?retryWrites=true&w=majority";
+const mongoConnectionURL = process.env.MONGO_SRV;
 // TODO change database name to the name you chose
 const databaseName = "unison";
 
@@ -59,7 +59,7 @@ app.use(express.json());
 app.use(
   session({
     // TODO: add a SESSION_SECRET string in your .env file, and replace the secret with process.env.SESSION_SECRET
-    secret: "session-secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
@@ -97,7 +97,7 @@ app.use((err, req, res, next) => {
 });
 
 // hardcode port to 3000 for now
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.Server(app);
 socketManager.init(server);
 
